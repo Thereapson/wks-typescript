dcdf94bebe87314a64fa6e2f37b2d560ec6ecfa1
sebastian <thereapson@gmail.com>
2023-01-16 11:50:20 -0500

    ejercicio4

---
+++ clases/homeworks/ejercicio 4.js
@@ -0,0 +1,21 @@
+"use strict";
+exports.__esModule = true;
+//tipado inferido
+var mati = {
+    nombre: 'Matias',
+    edad: 29
+};
+var franco = {
+    nombre: "Franco",
+    edad: 27
+};
+var diego = {
+    nombre: "Diego",
+    edad: 29,
+    esActivo: false //que pasa si comento alguno?
+};
+var Fede = {
+    name: 'Fede',
+    age: 29,
+    isActive: false
+};
